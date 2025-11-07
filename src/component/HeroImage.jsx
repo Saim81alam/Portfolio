0d4982a9d0b9019edb64bafc083662ca286dd2f8
Sameer Alam <s.a20198171@gmail.com>
2025-11-07 19:09:06 +0530

    amg

--- src/component/HeroImage.jsx
+++ src/component/HeroImage.jsx
@@ -3,7 +3,7 @@ export default function HeroImage() {
     <div className="flex items-center justify-center px-4 sm:px-0">
       <div className="relative w-full max-w-sm sm:max-w-md">
         <img
-          src="/portrait.jpg"
+          src="/Alam.png"
           alt="Temitayo Adebayo - Frontend Developer"
           className="w-full rounded-2xl border-2 sm:border-4 border-[#ff004d] shadow-2xl object-cover"
         />
