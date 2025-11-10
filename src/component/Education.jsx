875729bcc3a47edacc64fda736f71a462627ceb5
Sameer Alam <s.a20198171@gmail.com>
2025-11-10 20:12:37 +0530

    ahsg

--- src/component/Education.jsx
+++ src/component/Education.jsx
@@ -3,16 +3,10 @@ export default function Education() {
     {
       id: "1",
       institution: " Raja Mahendra Pratap Singh University, Aligarh",
+      course: "Bechulor Of Computer Science (BCA)",
       duration: "2021 - 2024",
       description:
         "Studying Software Engineering with focus on Frontend and Backend Development. Learning modern web technologies and best practices.",
-    },
-    {
-      id: "2",
-      institution: "University of Lagos",
-      duration: "2018 - 2022",
-      description:
-        "Bachelor of Science in Computer Science. Graduated with honors, specialized in web development and software engineering.",
     },
   ];
   return (
@@ -45,6 +39,9 @@ export default function Education() {
                   <h3 className="text-xl sm:text-2xl font-bold text-white mb-2 pr-8">
                     {entry.institution}
                   </h3>
+                  <h2 className="text-xl sm:text-xl font-bold text-white mb-2 pr-8">
+                    {entry.course}
+                  </h2>
                   <p className="text-[#ff004d] text-base sm:text-lg font-semibold mb-3 sm:mb-4">
                     {entry.duration}
                   </p>
